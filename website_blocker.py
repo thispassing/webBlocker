66b29dfe2918bea94a98536038dd3250870c0013
thispassing <thispassing@users.noreply.github.com>
2018-09-17 16:16:51 -0400

    working on loop

--- website_blocker.py
+++ website_blocker.py
@@ -1,13 +1,29 @@
 import time
 from datetime import datetime as dt
 
+hosts_temp="hosts"
 hosts_path=r"C:\Windows\System32\drivers\etc\hosts"
 redirect="127.0.0.1"
 website_list=["www.facebook.com", "facebook.com", "www.reddit.com", "reddit.com"]
 
 while True:
-    if dt(dt.now().year, dt.now().month, dt.now().day, 9) < dt.now() < dt(dt.now().year, dt.now().month, dt.now().day,19):
+    if dt(dt.now().year, dt.now().month, dt.now().day, 9) < dt.now() < dt(dt.now().year, dt.now().month, dt.now().day,23):
         print("Working Hours")
+        with open(hosts_temp, 'r+') as file:
+            content=file.read()
+            for website in website_list:
+                if website in content:
+                    pass
+                else:
+                    file.write(redirect+" "+website+"\n")
+
     else:
         print("Party Time!!")
+        with open(hosts_temp, 'r+') as file:
+            content=file.readlines()
+            file.seek
+            for line in content:
+                if not any(website in line for website in website_list):
+                    file.write(line)
+
     time.sleep(5)
