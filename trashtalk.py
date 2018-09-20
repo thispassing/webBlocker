90caacb3dd12c41e060740cd74a1a6fd7e21d1da
thispassing <thispassing@users.noreply.github.com>
2018-09-20 16:31:47 -0400

    finsihing practice writer, finished website blocker app

--- trashtalk.py
+++ trashtalk.py
@@ -3,13 +3,19 @@
 
 import time
 
-filter_list = ["bad", "suck"]
+filter_list = ["bad", "suck", "cy@"]
 age = int(input("Are you old enough? Enter your age: "))
+good_c = "cya"
+bad_c = "cy@"
+when_30 = "AHAHAHAHA\nPWNED\n"
+# quick = file.write(f"hey everyone\ni'm andy hehe\nyou are bad!!!!!!!!!!!!\ni win\ngg\n{good_c}\nyou suck haha!!!!!!!!!!\n{bad_c}")
 
 while True:
     if age < 18:
         with open("gamelog.txt", 'r+') as file:
             content=file.readlines()
+            file.seek(0)
+            file.write(f"hey everyone\ni'm andy hehe\nyou are bad!!!!!!!!!!!!\ni win\ngg\n{good_c}\nyou suck haha!!!!!!!!!!\n{bad_c}")
             file.seek(0)
             for line in content:
                 if not any(bad in line for bad in filter_list):
@@ -17,10 +23,18 @@ while True:
             file.truncate()
             print("done")
         break
+    elif age == 30:
+        with open("gamelog.txt", 'r+') as file:
+            content=file.read()
+            file.seek(0)
+            file.write(f"AHAHAHAHA\nPWNED\n{bad_c}")
+            file.truncate()
+            print("that was different")
+        break
     else:
         with open("gamelog.txt", 'r+') as file:
             file.seek(0)
-            file.write("hey everyone\ni'm andy hehe\nyou are bad!!!!!!!!!!!!\ni win\ngg\nyou suck haha!!!!!!!!!!\ncya")
+            file.write(f"hey everyone\ni'm andy hehe\nyou are bad!!!!!!!!!!!!\ni win\ngg\n{good_c}\nyou suck haha!!!!!!!!!!\n{bad_c}")
             file.truncate()
             print("done right")
         break
--- website_blocker.pyw
+++ website_blocker.pyw
@@ -4,20 +4,21 @@ from datetime import datetime as dt
 new_host="hosts"
 hosts_path=r"C:\Windows\System32\drivers\etc\hosts"
 redirect="127.0.0.1"
-website_list=["www.facebook.com", "facebook.com", "www.reddit.com", "reddit.com"]
+website_list=["www.facebook.com", "facebook.com", "www.reddit.com", "reddit.com", "mssplus.mcafee.com", "twitch.tv", "www.twitch.tv"]
 
 while True:
     if dt(dt.now().year, dt.now().month, dt.now().day, 9) < dt.now() < dt(dt.now().year, dt.now().month, dt.now().day,12):
         print("Working Hours")
-        with open(new_host, 'r+') as file:
+        with open(hosts_path, 'r+') as file:
             content=file.read()
             for website in website_list:
                 if website in content:
                     pass
                 else:
                     file.write(redirect+" "+website+"\n")
+    time.sleep(300)
     else:
-        with open(new_host, 'r+') as file:
+        with open(hosts_path, 'r+') as file:
             content=file.readlines()
             file.seek(0)
             for line in content:
@@ -25,4 +26,5 @@ while True:
                     file.write(line)
             file.truncate()
         print("Party Time!!")
-    time.sleep(5)
+        break
+    time.sleep(300)
